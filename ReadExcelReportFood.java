de16d013eb523be5524bd5f3e4766ff3c375f275
Rith <rithronlkh@gmail.com>
2018-06-27 10:25:07 +0700

    add java class

---
+++ ReadExcelReportFood.java
@@ -0,0 +1,52 @@
+package com.nan.bank.controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+
+public class ReadExcelReportFood {
+	 public static void main(String[] args) throws IOException {
+		 	String excelFilePath = "D:\\rdbws\\workspace\\HRManagement\\WebContent\\images\\01.12.2017 Food.xls";
+	        
+	        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+	         
+	        //Workbook workbook = new XSSFWorkbook(inputStream);
+	        Workbook workbook = new HSSFWorkbook(inputStream);
+	        Sheet firstSheet = workbook.getSheetAt(0);
+	        Iterator<Row> iterator = firstSheet.iterator();
+	         
+	        while (iterator.hasNext()) {
+	            Row nextRow = iterator.next();
+	            Iterator<Cell> cellIterator = nextRow.cellIterator();
+	             
+	            while (cellIterator.hasNext()) {
+	                Cell cell = cellIterator.next();
+	                 
+	                switch (cell.getCellType()) {
+	                    case Cell.CELL_TYPE_STRING:
+	                        System.out.print(cell.getStringCellValue());
+	                        break;
+	                    case Cell.CELL_TYPE_BOOLEAN:
+	                        System.out.print(cell.getBooleanCellValue());
+	                        break;
+	                    case Cell.CELL_TYPE_NUMERIC:
+	                        System.out.print(cell.getNumericCellValue());
+	                        break;
+	                }
+	                System.out.print(" - ");
+	            }
+	            System.out.println();
+	        }
+	         
+	        workbook.close();
+	        inputStream.close();
+	    }
+}
